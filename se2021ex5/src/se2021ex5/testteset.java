f2a5c316b3c939b3155d22a28c94af510945bcce
Tsujii <r202003471ze@jindai.jp>
2022-01-24 20:12:32 +0900

    version3

--- se2021ex5/src/se2021ex5/testteset.java
+++ se2021ex5/src/se2021ex5/testteset.java
@@ -3,7 +3,7 @@ package se2021ex5;
 public class testteset {
 
 	public static void main(String[] args) {
-		System.out.println("uoooooooooooookonnnichiha1.");
+		System.out.println("uoooooooooooookonnbannwa1.");
 	}
 
 }
